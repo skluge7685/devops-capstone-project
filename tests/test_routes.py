9959f7ad78e2558e8d7baa8e033ac1404a717912
skluge7685 <se.kluge@outlook.de>
2025-05-24 16:01:47 -0400

    added not allowed

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -166,3 +166,4 @@ class TestAccountService(TestCase):
         account = self._create_accounts(1)[0]
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
